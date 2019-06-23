6b0fa0e016e95aba2bb00a45a4851529f20320da
Ievgenii Lopushen <lopushen@gmail.com>
2019-06-24 01:09:08 +0300

    javascript template expressions is also fun!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,10 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<app-servers></app-servers>
+  <app-servers></app-servers>
+  <app-servers></app-servers>
+  <app-servers></app-servers>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
