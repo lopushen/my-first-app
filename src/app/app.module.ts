4b09475dc35dc14b97f188d254d9809d0f5b2e52
Ievgenii Lopushen <lopushen@gmail.com>
2019-06-24 00:55:07 +0300

    added new (Server) component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ServerComponent} from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ServerComponent
   ],
-  imports: [
+  imports: [ // allows to add other modules, custom or from Angular, like http
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // lists all the components
 })
 export class AppModule { }
