9f823958ea0c4da88acc32882d139b19582de3a0
Ievgenii Lopushen <lopushen@gmail.com>
2019-06-24 01:04:48 +0300

    well, template can also be used

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<app-servers></app-servers>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
