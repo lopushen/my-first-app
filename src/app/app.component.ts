5070f94d831a0811bae1af5d2724b2bb1c852b9c
Ievgenii Lopushen <lopushen@gmail.com>
2019-06-25 01:54:35 +0300

    but still external css file is better

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles: [`
-    h3 {
-      color: aquamarine;
-    }
-  `]
+   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-first-app';
