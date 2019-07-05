2a859303cebfc253164a4ff5a5d59b79fd510a24
Ievgenii Lopushen <lopushen@gmail.com>
2019-07-05 23:57:44 +0300

    dynamic styling with ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,7 +8,15 @@ export class ServerComponent {
   serverId = 10;
   serverStatus = 'ONLINE';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'ONLINE' : 'OFFLINE';
+  }
+
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'ONLINE' ? 'green' : 'red';
   }
 }
